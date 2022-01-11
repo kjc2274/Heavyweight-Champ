701f8804c18830e3e677c038fac934b61bc2b40d
Kevin Cleary <kjc2274@yahoo.com>
2022-01-11 12:49:41 -0500

    did some work on the homepage

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -2,12 +2,18 @@ import { Link } from "react-router-dom";
 
 export default function Homepage() {
     return (
-        <div>
+        <div id="homepage">
             <div>
-                <Link to="/select">Fight</Link>
+                <Link to="/select">
+                    <img className="homeImage" src="https://freerangestock.com/sample/116676/boxing-match-silhouette-.jpg" alt="boxing silhouette"/>
+                    <h3>Fight</h3>
+                </Link>
             </div>
             <div>
-                <Link to = "/create-fighter">Create Custom Opponent</Link>
+                <Link to = "/create-fighter">
+                <img className="homeImage" src="https://freerangestock.com/sample/116066/boxing-silhouette-.jpg" alt="boxing silhouette"/>
+                    <h3>Create Custom Opponent</h3>
+                </Link>
             </div>
         </div>
     )
