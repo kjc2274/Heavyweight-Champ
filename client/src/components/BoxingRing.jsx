5902e355f95aa4595d7881747f686338c0a48416
Kevin Cleary <kjc2274@yahoo.com>
2022-01-11 13:20:02 -0500

    populates fighter list from airtable

--- client/src/components/BoxingRing.jsx
+++ client/src/components/BoxingRing.jsx
@@ -1,3 +1,4 @@
+import api from "../services/apiConfig";
 
 
 export default function BoxingRing() {
--- client/src/components/CustomOpponents.jsx
+++ client/src/components/CustomOpponents.jsx
@@ -1,9 +1,38 @@
-
+import api from "../services/apiConfig";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function CustomOpponents() {
+    const [fighters, setFighters] = useState([]);
+
+    useEffect(()=>{
+        const fetchFighters = async () => {
+            const res = await api.get();
+            console.log(res.data.records);
+            setFighters(res.data.records);
+        }
+        fetchFighters();
+    }, [])
+
     return (
         <div>
             <h2>Custom Opponents</h2>
+            <ul>
+                {fighters.map((fighter)=>{
+                    return(
+                        <li key={fighter.id}>
+                            <Link to={`/ring/${fighter.id}`}>
+                            <div>
+                                <img style={{ height: "20vh"}} src={fighter.fields.image} alt={fighter.fields.name}/>
+                                <h3>{fighter.fields.name}</h3>
+                                <h4>{fighter.fields.hp}</h4>
+                            </div>
+                            </Link>
+                        </li>
+                    )
+                })}
+                
+            </ul>
         </div>
     )
 }
