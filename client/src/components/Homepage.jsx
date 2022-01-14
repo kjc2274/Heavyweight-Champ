e7b00b03dfb9cd6a5cc5971c740aad2f63512315
Kevin Cleary <kjc2274@yahoo.com>
2022-01-13 20:05:23 -0500

    finished homepage styling

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -10,19 +10,24 @@ export default function Homepage() {
             <HelpButton setVisible={setVisible}/>
             <div>
                 <Link to="/fighters">
-                    <img className="homeImage" src="https://freerangestock.com/sample/116676/boxing-match-silhouette-.jpg" alt="boxing silhouette"/>
+                <p className={myClass}>
+                    Do you have what it takes to be the next heavyweight champ? Click here to choose a challenger.
+                </p>
+                    <img className="homeImage" 
+                    src="https://freerangestock.com/sample/116676/boxing-match-silhouette-.jpg" 
+                    alt="boxing silhouette"/>
                     <h3>Fight</h3>
                 </Link>
             </div>
             <div>
                 <Link to = "/create-fighter">
+                <p className={myClass}>
+                    Click here to create a custom opponent, and have that opponent added to the "Fight" selection screen.
+                </p>
                 <img className="homeImage" src="https://freerangestock.com/sample/116066/boxing-silhouette-.jpg" alt="boxing silhouette"/>
                     <h3>Create Custom Opponent</h3>
                 </Link>
             </div>
-            <p className={myClass}>
-                Do you have what it takes to be the next heavyweight champ?
-            </p>
         </div>
     )
 }
